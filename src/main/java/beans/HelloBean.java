f8b52a1b121c8ea84227b7bdba74777bf9e62ba5
marioosh <marioosh@963358be-e051-4187-90f4-1164e00299d6>
2010-09-03 10:15:19 +0000

    git-svn-id: file:///var/svn/java/jsf2_maven@287 963358be-e051-4187-90f4-1164e00299d6

--- src/main/java/beans/HelloBean.java
+++ src/main/java/beans/HelloBean.java
@@ -3,9 +3,16 @@ package beans;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-
 @ManagedBean
 @RequestScoped
 public class HelloBean {
+	private String message;
 
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
