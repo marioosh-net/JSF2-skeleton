bcf1dae147734c2e4e2b3d7df53a4efa1049b1f1
marioosh <marioosh@963358be-e051-4187-90f4-1164e00299d6>
2010-09-08 19:52:45 +0000

    git-svn-id: file:///var/svn/java/jsf2_maven@314 963358be-e051-4187-90f4-1164e00299d6

--- src/main/java/beans/HelloBean.java
+++ src/main/java/beans/HelloBean.java
@@ -4,15 +4,21 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import org.apache.log4j.Logger;
 
 @ManagedBean
 @RequestScoped
 public class HelloBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+	private transient Logger log = Logger.getLogger(getClass());
 	private String message = "Hello World";
 
+	
+	public HelloBean() {
+		log.debug("HelloBean()");
+	}
+	
 	public String getMessage() {
 		return message;
 	}
